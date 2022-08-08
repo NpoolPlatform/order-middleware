5483e520a3a877a3f1255aad93e69dcd00f17468
Zhao KK <kikakkz@hotmail.com>
2022-08-08 09:30:08 +0000

    For safe update order

--- pkg/order/update.go
+++ pkg/order/update.go
@@ -5,8 +5,12 @@ import (
 	"fmt"
 
 	paymentcrud "github.com/NpoolPlatform/cloud-hashing-order/pkg/crud/payment"
+
 	"github.com/NpoolPlatform/cloud-hashing-order/pkg/db/ent"
+	"github.com/NpoolPlatform/cloud-hashing-order/pkg/db/ent/payment"
 	"github.com/NpoolPlatform/order-middleware/pkg/db"
+
+	orderconst "github.com/NpoolPlatform/cloud-hashing-order/pkg/const"
 
 	orderpb "github.com/NpoolPlatform/message/npool/cloud-hashing-order"
 	npool "github.com/NpoolPlatform/message/npool/order/mw/v1/order"
@@ -25,10 +29,25 @@ func UpdateOrder(ctx context.Context, in *npool.OrderReq) (info *npool.Order, er
 		return nil, fmt.Errorf("invalid order")
 	}
 
-	err = db.WithClient(ctx, func(ctx context.Context, cli *ent.Client) error {
-		_, err = cli.
+	err = db.WithTx(ctx, func(ctx context.Context, tx *ent.Tx) error {
+		info, err := tx.
 			Payment.
-			UpdateOneID(uuid.MustParse(in.GetPaymentID())).
+			Query().
+			Where(
+				payment.ID(uuid.MustParse(in.GetPaymentID())),
+			).
+			ForUpdate().
+			Only(ctx)
+		if err != nil {
+			return err
+		}
+
+		if info.State != orderconst.PaymentStateWait {
+			return fmt.Errorf("not wait payment")
+		}
+
+		_, err = info.
+			Update().
 			SetUserSetCanceled(in.GetCanceled()).
 			Save(ctx)
 		return err
