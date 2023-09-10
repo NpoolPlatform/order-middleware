0dc718973845796b21b74692342517adb944e820
root <root@development-box-1.development-box.kube-system.svc.cluster.local>
2023-09-10 11:11:52 +0000

    fix ut

--- pkg/mw/outofgas/outofgas_test.go
+++ pkg/mw/outofgas/outofgas_test.go
@@ -215,6 +215,17 @@ func setup(t *testing.T) func(*testing.T) {
 	_, err = h15.UpdateOrder(context.Background())
 	assert.Nil(t, err)
 
+	order.OrderState = ordertypes.OrderState_OrderStatePaymentUnlockAccount
+	h20, err := order1.NewHandler(
+		context.Background(),
+		order1.WithID(&order.ID, true),
+		order1.WithOrderState(&order.OrderState, true),
+	)
+	assert.Nil(t, err)
+	_, err = h20.UpdateOrder(context.Background())
+	assert.Nil(t, err)
+
+	order.OrderState = ordertypes.OrderState_OrderStateTransferGoodStockWaitStart
 	order.OrderState = ordertypes.OrderState_OrderStatePaid
 	h16, err := order1.NewHandler(
 		context.Background(),
