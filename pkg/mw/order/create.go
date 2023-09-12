fda3f3db5408a7b34a65abf90ea3a5f9ec1e3fa2
Zhao KK <kikakkz@hotmail.com>
2023-09-12 02:31:45 +0000

    Correct no payment payment state

--- pkg/mw/order/create.go
+++ pkg/mw/order/create.go
@@ -24,6 +24,9 @@ type createHandler struct {
 }
 
 func (h *createHandler) paymentState(req *ordercrud.Req) *types.PaymentState {
+	if req.PaymentType != nil && *req.PaymentType == types.PaymentType_PayWithNoPayment {
+		return types.PaymentState_PaymentStateNoPayment.Enum()
+	}
 	if (req.TransferAmount != nil && req.TransferAmount.Cmp(decimal.NewFromInt(0)) > 0) ||
 		(req.BalanceAmount != nil && req.BalanceAmount.Cmp(decimal.NewFromInt(0)) > 0) {
 		return types.PaymentState_PaymentStateWait.Enum()
