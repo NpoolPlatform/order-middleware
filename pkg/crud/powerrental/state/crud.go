3b638f008c01205937682d835fba68682196e31c
Zhao KK <kikakkz@hotmail.com>
2024-04-17 07:34:55 +0000

    Add power rental state

--- pkg/crud/powerrental/state/crud.go
+++ pkg/crud/powerrental/state/crud.go
@@ -1,4 +1,4 @@
-package orderstate
+package powerrentalstate
 
 import (
 	"fmt"
@@ -6,64 +6,41 @@ import (
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	types "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
 	"github.com/NpoolPlatform/order-middleware/pkg/db/ent"
-	entorderstate "github.com/NpoolPlatform/order-middleware/pkg/db/ent/orderstate"
+	entpowerrentalstate "github.com/NpoolPlatform/order-middleware/pkg/db/ent/powerrentalstate"
+
 	"github.com/google/uuid"
-	"github.com/shopspring/decimal"
 )
 
 type Req struct {
-	EntID                *uuid.UUID
-	OrderID              *uuid.UUID
-	OrderState           *types.OrderState
-	CancelState          *types.OrderState
-	StartMode            *types.OrderStartMode
-	StartAt              *uint32
-	EndAt                *uint32
-	PaidAt               *uint32
-	LastBenefitAt        *uint32
-	BenefitState         *types.BenefitState
-	UserSetPaid          *bool
-	UserSetCanceled      *bool
-	AdminSetCanceled     *bool
-	PaymentTransactionID *string
-	PaymentFinishAmount  *decimal.Decimal
-	PaymentState         *types.PaymentState
-	OutOfGasHours        *uint32
-	CompensateHours      *uint32
-	RenewState           *types.OrderRenewState
-	RenewNotifyAt        *uint32
-	CreatedAt            *uint32
-	DeletedAt            *uint32
+	EntID            *uuid.UUID
+	OrderID          *uuid.UUID
+	CancelState      *types.OrderState
+	EndAt            *uint32
+	PaidAt           *uint32
+	UserSetPaid      *bool
+	UserSetCanceled  *bool
+	AdminSetCanceled *bool
+	PaymentState     *types.PaymentState
+	OutOfGasHours    *uint32
+	CompensateHours  *uint32
+	RenewState       *types.OrderRenewState
+	RenewNotifyAt    *uint32
+	DeletedAt        *uint32
 }
 
 //nolint:gocyclo
-func CreateSet(c *ent.OrderStateCreate, req *Req) *ent.OrderStateCreate {
+func CreateSet(c *ent.PowerRentalStateCreate, req *Req) *ent.PowerRentalStateCreate {
 	if req.EntID != nil {
 		c.SetEntID(*req.EntID)
 	}
 	if req.OrderID != nil {
 		c.SetOrderID(*req.OrderID)
 	}
-	if req.OrderState != nil {
-		c.SetOrderState(req.OrderState.String())
-	}
 	if req.CancelState != nil {
 		c.SetCancelState(req.CancelState.String())
 	}
-	if req.StartMode != nil {
-		c.SetStartMode(req.StartMode.String())
-	}
-	if req.StartAt != nil {
-		c.SetStartAt(*req.StartAt)
-	}
 	if req.EndAt != nil {
 		c.SetEndAt(*req.EndAt)
-	}
-	if req.LastBenefitAt != nil {
-		c.SetLastBenefitAt(*req.LastBenefitAt)
-	}
-	if req.BenefitState != nil {
-		c.SetBenefitState(req.BenefitState.String())
 	}
 	if req.UserSetPaid != nil {
 		c.SetUserSetPaid(*req.UserSetPaid)
@@ -73,12 +50,6 @@ func CreateSet(c *ent.OrderStateCreate, req *Req) *ent.OrderStateCreate {
 	}
 	if req.AdminSetCanceled != nil {
 		c.SetAdminSetCanceled(*req.AdminSetCanceled)
-	}
-	if req.PaymentTransactionID != nil {
-		c.SetPaymentTransactionID(*req.PaymentTransactionID)
-	}
-	if req.PaymentFinishAmount != nil {
-		c.SetPaymentFinishAmount(*req.PaymentFinishAmount)
 	}
 	if req.PaymentState != nil {
 		c.SetPaymentState(req.PaymentState.String())
@@ -95,38 +66,19 @@ func CreateSet(c *ent.OrderStateCreate, req *Req) *ent.OrderStateCreate {
 	if req.RenewNotifyAt != nil {
 		c.SetRenewNotifyAt(*req.RenewNotifyAt)
 	}
-	if req.CreatedAt != nil {
-		c.SetCreatedAt(*req.CreatedAt)
-	}
-
 	return c
 }
 
 //nolint:gocyclo
-func UpdateSet(u *ent.OrderStateUpdateOne, req *Req) *ent.OrderStateUpdateOne {
-	if req.OrderState != nil {
-		u.SetOrderState(req.OrderState.String())
-	}
+func UpdateSet(u *ent.PowerRentalStateUpdateOne, req *Req) *ent.PowerRentalStateUpdateOne {
 	if req.CancelState != nil {
 		u.SetCancelState(req.CancelState.String())
-	}
-	if req.StartMode != nil {
-		u.SetStartMode(req.StartMode.String())
-	}
-	if req.StartAt != nil {
-		u.SetStartAt(*req.StartAt)
 	}
 	if req.EndAt != nil {
 		u.SetEndAt(*req.EndAt)
 	}
 	if req.PaidAt != nil {
 		u.SetPaidAt(*req.PaidAt)
-	}
-	if req.LastBenefitAt != nil {
-		u.SetLastBenefitAt(*req.LastBenefitAt)
-	}
-	if req.BenefitState != nil {
-		u.SetBenefitState(req.BenefitState.String())
 	}
 	if req.UserSetPaid != nil {
 		u.SetUserSetPaid(*req.UserSetPaid)
@@ -136,12 +88,6 @@ func UpdateSet(u *ent.OrderStateUpdateOne, req *Req) *ent.OrderStateUpdateOne {
 	}
 	if req.AdminSetCanceled != nil {
 		u.SetAdminSetCanceled(*req.AdminSetCanceled)
-	}
-	if req.PaymentTransactionID != nil {
-		u.SetPaymentTransactionID(*req.PaymentTransactionID)
-	}
-	if req.PaymentFinishAmount != nil {
-		u.SetPaymentFinishAmount(*req.PaymentFinishAmount)
 	}
 	if req.PaymentState != nil {
 		u.SetPaymentState(req.PaymentState.String())
@@ -165,55 +111,21 @@ func UpdateSet(u *ent.OrderStateUpdateOne, req *Req) *ent.OrderStateUpdateOne {
 }
 
 type Conds struct {
-	EntID                *cruder.Cond
-	EntIDs               *cruder.Cond
-	ID                   *cruder.Cond
-	IDs                  *cruder.Cond
-	OrderID              *cruder.Cond
-	OrderState           *cruder.Cond
-	StartMode            *cruder.Cond
-	LastBenefitAt        *cruder.Cond
-	BenefitState         *cruder.Cond
-	PaymentTransactionID *cruder.Cond
-	PaymentState         *cruder.Cond
-	OrderStates          *cruder.Cond
-	RenewState           *cruder.Cond
-	RenewNotifyAt        *cruder.Cond
+	ID            *cruder.Cond
+	IDs           *cruder.Cond
+	EntID         *cruder.Cond
+	EntIDs        *cruder.Cond
+	OrderID       *cruder.Cond
+	PaymentState  *cruder.Cond
+	RenewState    *cruder.Cond
+	RenewNotifyAt *cruder.Cond
 }
 
 //nolint
-func SetQueryConds(q *ent.OrderStateQuery, conds *Conds) (*ent.OrderStateQuery, error) {
-	q.Where(entorderstate.DeletedAt(0))
+func SetQueryConds(q *ent.PowerRentalStateQuery, conds *Conds) (*ent.PowerRentalStateQuery, error) {
+	q.Where(entpowerrentalstate.DeletedAt(0))
 	if conds == nil {
 		return q, nil
-	}
-	if conds.EntID != nil {
-		id, ok := conds.EntID.Val.(uuid.UUID)
-		if !ok {
-			return nil, fmt.Errorf("invalid entid")
-		}
-		switch conds.EntID.Op {
-		case cruder.EQ:
-			q.Where(entorderstate.EntID(id))
-		case cruder.NEQ:
-			q.Where(entorderstate.EntIDNEQ(id))
-		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.EntIDs != nil {
-		ids, ok := conds.EntIDs.Val.([]uuid.UUID)
-		if !ok {
-			return nil, fmt.Errorf("invalid entids")
-		}
-		if len(ids) > 0 {
-			switch conds.EntIDs.Op {
-			case cruder.IN:
-				q.Where(entorderstate.EntIDIn(ids...))
-			default:
-				return nil, fmt.Errorf("invalid order field")
-			}
-		}
 	}
 	if conds.ID != nil {
 		id, ok := conds.ID.Val.(uint32)
@@ -222,9 +134,9 @@ func SetQueryConds(q *ent.OrderStateQuery, conds *Conds) (*ent.OrderStateQuery, 
 		}
 		switch conds.ID.Op {
 		case cruder.EQ:
-			q.Where(entorderstate.ID(id))
+			q.Where(entpowerrentalstate.ID(id))
 		default:
-			return nil, fmt.Errorf("invalid order field")
+			return nil, fmt.Errorf("invalid powerrental field")
 		}
 	}
 	if conds.IDs != nil {
@@ -235,58 +147,50 @@ func SetQueryConds(q *ent.OrderStateQuery, conds *Conds) (*ent.OrderStateQuery, 
 		if len(ids) > 0 {
 			switch conds.IDs.Op {
 			case cruder.IN:
-				q.Where(entorderstate.IDIn(ids...))
+				q.Where(entpowerrentalstate.IDIn(ids...))
 			default:
-				return nil, fmt.Errorf("invalid order field")
+				return nil, fmt.Errorf("invalid powerrental field")
+			}
+		}
+	}
+	if conds.EntID != nil {
+		id, ok := conds.EntID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid entid")
+		}
+		switch conds.EntID.Op {
+		case cruder.EQ:
+			q.Where(entpowerrentalstate.EntID(id))
+		case cruder.NEQ:
+			q.Where(entpowerrentalstate.EntIDNEQ(id))
+		default:
+			return nil, fmt.Errorf("invalid powerrental field")
+		}
+	}
+	if conds.EntIDs != nil {
+		ids, ok := conds.EntIDs.Val.([]uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid entids")
+		}
+		if len(ids) > 0 {
+			switch conds.EntIDs.Op {
+			case cruder.IN:
+				q.Where(entpowerrentalstate.EntIDIn(ids...))
+			default:
+				return nil, fmt.Errorf("invalid powerrental field")
 			}
 		}
 	}
 	if conds.OrderID != nil {
 		id, ok := conds.OrderID.Val.(uuid.UUID)
 		if !ok {
-			return nil, fmt.Errorf("invalid orderid")
+			return nil, fmt.Errorf("invalid powerrentalid")
 		}
 		switch conds.OrderID.Op {
 		case cruder.EQ:
-			q.Where(entorderstate.OrderID(id))
+			q.Where(entpowerrentalstate.OrderID(id))
 		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.OrderState != nil {
-		state, ok := conds.OrderState.Val.(types.OrderState)
-		if !ok {
-			return nil, fmt.Errorf("invalid orderstate")
-		}
-		switch conds.OrderState.Op {
-		case cruder.EQ:
-			q.Where(entorderstate.OrderState(state.String()))
-		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.StartMode != nil {
-		startmode, ok := conds.StartMode.Val.(types.OrderStartMode)
-		if !ok {
-			return nil, fmt.Errorf("invalid startmode")
-		}
-		switch conds.StartMode.Op {
-		case cruder.EQ:
-			q.Where(entorderstate.StartMode(startmode.String()))
-		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.BenefitState != nil {
-		state, ok := conds.BenefitState.Val.(types.BenefitState)
-		if !ok {
-			return nil, fmt.Errorf("invalid benefitstate")
-		}
-		switch conds.BenefitState.Op {
-		case cruder.EQ:
-			q.Where(entorderstate.BenefitState(state.String()))
-		default:
-			return nil, fmt.Errorf("invalid order field")
+			return nil, fmt.Errorf("invalid powerrental field")
 		}
 	}
 	if conds.PaymentState != nil {
@@ -296,47 +200,9 @@ func SetQueryConds(q *ent.OrderStateQuery, conds *Conds) (*ent.OrderStateQuery, 
 		}
 		switch conds.PaymentState.Op {
 		case cruder.EQ:
-			q.Where(entorderstate.PaymentState(state.String()))
+			q.Where(entpowerrentalstate.PaymentState(state.String()))
 		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.PaymentTransactionID != nil {
-		paymentTransactionID, ok := conds.PaymentTransactionID.Val.(string)
-		if !ok {
-			return nil, fmt.Errorf("invalid paymenttransactionid")
-		}
-		switch conds.PaymentTransactionID.Op {
-		case cruder.EQ:
-			q.Where(entorderstate.PaymentTransactionID(paymentTransactionID))
-		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.LastBenefitAt != nil {
-		lastBenefitAt, ok := conds.LastBenefitAt.Val.(uint32)
-		if !ok {
-			return nil, fmt.Errorf("invalid lastbenefitat")
-		}
-		switch conds.LastBenefitAt.Op {
-		case cruder.EQ:
-			q.Where(entorderstate.LastBenefitAt(lastBenefitAt))
-		default:
-			return nil, fmt.Errorf("invalid order field")
-		}
-	}
-	if conds.OrderStates != nil {
-		states, ok := conds.OrderStates.Val.([]string)
-		if !ok {
-			return nil, fmt.Errorf("invalid orderstates")
-		}
-		if len(states) > 0 {
-			switch conds.OrderStates.Op {
-			case cruder.IN:
-				q.Where(entorderstate.OrderStateIn(states...))
-			default:
-				return nil, fmt.Errorf("invalid order field")
-			}
+			return nil, fmt.Errorf("invalid powerrental field")
 		}
 	}
 	if conds.RenewState != nil {
@@ -346,9 +212,9 @@ func SetQueryConds(q *ent.OrderStateQuery, conds *Conds) (*ent.OrderStateQuery, 
 		}
 		switch conds.RenewState.Op {
 		case cruder.EQ:
-			q.Where(entorderstate.RenewState(state.String()))
+			q.Where(entpowerrentalstate.RenewState(state.String()))
 		default:
-			return nil, fmt.Errorf("invalid order field")
+			return nil, fmt.Errorf("invalid powerrental field")
 		}
 	}
 	if conds.RenewNotifyAt != nil {
@@ -358,17 +224,17 @@ func SetQueryConds(q *ent.OrderStateQuery, conds *Conds) (*ent.OrderStateQuery, 
 		}
 		switch conds.RenewNotifyAt.Op {
 		case cruder.EQ:
-			q.Where(entorderstate.RenewNotifyAt(at))
+			q.Where(entpowerrentalstate.RenewNotifyAt(at))
 		case cruder.LT:
-			q.Where(entorderstate.RenewNotifyAtLT(at))
+			q.Where(entpowerrentalstate.RenewNotifyAtLT(at))
 		case cruder.LTE:
-			q.Where(entorderstate.RenewNotifyAtLTE(at))
+			q.Where(entpowerrentalstate.RenewNotifyAtLTE(at))
 		case cruder.GT:
-			q.Where(entorderstate.RenewNotifyAtGT(at))
+			q.Where(entpowerrentalstate.RenewNotifyAtGT(at))
 		case cruder.GTE:
-			q.Where(entorderstate.RenewNotifyAtGTE(at))
+			q.Where(entpowerrentalstate.RenewNotifyAtGTE(at))
 		default:
-			return nil, fmt.Errorf("invalid order field")
+			return nil, fmt.Errorf("invalid powerrental field")
 		}
 	}
 	return q, nil
