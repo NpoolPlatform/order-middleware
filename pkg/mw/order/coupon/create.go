8855fca1bcab130c75f8e57e35f467b8f549c806
Zhao KK <kikakkz@hotmail.com>
2024-04-18 06:44:03 +0000

    Add coupon construct

--- pkg/mw/order/coupon/create.go
+++ pkg/mw/order/coupon/create.go
@@ -3,7 +3,6 @@ package ordercoupon
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/NpoolPlatform/order-middleware/pkg/db"
 	"github.com/NpoolPlatform/order-middleware/pkg/db/ent"
@@ -17,45 +16,7 @@ type createHandler struct {
 }
 
 func (h *createHandler) constructSQL() {
-	comma := ""
-	now := uint32(time.Now().Unix())
-
-	_sql := "insert into order_coupons "
-	_sql += "("
-	if h.EntID != nil {
-		_sql += "ent_id"
-		comma = ", "
-	}
-	_sql += comma + "order_id"
-	comma = ", "
-	_sql += comma + "coupon_id"
-	_sql += comma + "created_at"
-	_sql += comma + "updated_at"
-	_sql += comma + "deleted_at"
-	_sql += ")"
-	comma = ""
-	_sql += " select * from (select "
-	if h.EntID != nil {
-		_sql += fmt.Sprintf("'%v' as ent_id ", *h.EntID)
-		comma = ", "
-	}
-	_sql += fmt.Sprintf("%v'%v' as order_id", comma, *h.OrderID)
-	comma = ", "
-	_sql += fmt.Sprintf("%v'%v' as coupon_id", comma, *h.CouponID)
-	_sql += fmt.Sprintf("%v%v as created_at", comma, now)
-	_sql += fmt.Sprintf("%v%v as updated_at", comma, now)
-	_sql += fmt.Sprintf("%v0 as deleted_at", comma)
-	_sql += ") as tmp "
-	_sql += "where not exists ("
-	_sql += "select 1 from order_coupons "
-	_sql += fmt.Sprintf("where order_id = '%v' ", *h.OrderID)
-	_sql += fmt.Sprintf("and coupon_id = '%v' ", *h.CouponID)
-	_sql += " limit 1) and exists ("
-	_sql += "select 1 from order_bases "
-	_sql += fmt.Sprintf("where ent_id = '%v' ", *h.OrderID)
-	_sql += "limit 1)"
-
-	h.sql = _sql
+	h.sql = h.ConstructCreateSQL()
 }
 
 func (h *createHandler) createOrderCoupon(ctx context.Context, tx *ent.Tx) error {
