ac7934ca58bc08dc74f6124dd606a4ba1dcd0fd1
Zhao KK <kikakkz@hotmail.com>
2024-04-19 13:02:30 +0000

    Add base query

--- pkg/mw/fee/basequery.go
+++ pkg/mw/fee/basequery.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 
 	"entgo.io/ent/dialect/sql"
+	types "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
 	orderbasecrud "github.com/NpoolPlatform/order-middleware/pkg/crud/order/orderbase"
 	"github.com/NpoolPlatform/order-middleware/pkg/db/ent"
 	entfeeorder "github.com/NpoolPlatform/order-middleware/pkg/db/ent/feeorder"
+	entfeeorderstate "github.com/NpoolPlatform/order-middleware/pkg/db/ent/feeorderstate"
 	entorderbase "github.com/NpoolPlatform/order-middleware/pkg/db/ent/orderbase"
-	// entordercoupon "github.com/NpoolPlatform/order-middleware/pkg/db/ent/ordercoupon"
+	entorderlock "github.com/NpoolPlatform/order-middleware/pkg/db/ent/orderlock"
+	entorderstatebase "github.com/NpoolPlatform/order-middleware/pkg/db/ent/orderstatebase"
+	entpaymentbase "github.com/NpoolPlatform/order-middleware/pkg/db/ent/paymentbase"
 
 	"github.com/google/uuid"
 )
@@ -95,7 +99,7 @@ func (h *baseQueryHandler) queryJoinMyself(s *sql.Selector) {
 	)
 }
 
-func (h *baseQueryHandler) queryJoinFeeOrder(s *sql.Selector) error { //nolint
+func (h *baseQueryHandler) queryJoinFeeOrder(s *sql.Selector) {
 	t := sql.Table(entfeeorder.Table)
 	s.Join(t).
 		On(
@@ -113,12 +117,105 @@ func (h *baseQueryHandler) queryJoinFeeOrder(s *sql.Selector) error { //nolint
 		t.C(entfeeorder.FieldPromotionID),
 		t.C(entfeeorder.FieldDurationSeconds),
 	)
+}
+
+func (h *baseQueryHandler) queryJoinOrderStateBase(s *sql.Selector) error {
+	t := sql.Table(entorderstatebase.Table)
+	s.Join(t).
+		On(
+			s.C(entorderbase.FieldEntID),
+			t.C(entorderstatebase.FieldOrderID),
+		)
+	if h.OrderStateBaseConds.PaymentType != nil {
+		_type, ok := h.OrderStateBaseConds.PaymentType.Val.(types.PaymentType)
+		if !ok {
+			return fmt.Errorf("invalid paymenttype")
+		}
+		s.OnP(
+			sql.EQ(t.C(entorderstatebase.FieldPaymentType), _type.String()),
+		)
+	}
+	if h.OrderStateBaseConds.OrderState != nil {
+		_state, ok := h.OrderStateBaseConds.OrderState.Val.(types.OrderState)
+		if !ok {
+			return fmt.Errorf("invalid orderstate")
+		}
+		s.OnP(
+			sql.EQ(t.C(entorderstatebase.FieldOrderState), _state.String()),
+		)
+	}
+	s.AppendSelect(
+		t.C(entorderstatebase.FieldPaymentType),
+		t.C(entorderstatebase.FieldOrderState),
+	)
 	return nil
+}
+
+func (h *baseQueryHandler) queryJoinFeeOrderState(s *sql.Selector) error {
+	t := sql.Table(entfeeorderstate.Table)
+	s.Join(t).
+		On(
+			s.C(entorderbase.FieldEntID),
+			t.C(entfeeorderstate.FieldOrderID),
+		)
+	if h.FeeOrderStateConds.PaymentState != nil {
+		_state, ok := h.FeeOrderStateConds.PaymentState.Val.(types.PaymentState)
+		if !ok {
+			return fmt.Errorf("invalid paymentstate")
+		}
+		s.OnP(
+			sql.EQ(t.C(entfeeorderstate.FieldPaymentState), _state.String()),
+		)
+	}
+	s.AppendSelect(
+		t.C(entfeeorderstate.FieldPaymentID),
+		t.C(entfeeorderstate.FieldPaidAt),
+		t.C(entfeeorderstate.FieldUserSetPaid),
+		t.C(entfeeorderstate.FieldUserSetCanceled),
+		t.C(entfeeorderstate.FieldAdminSetCanceled),
+		t.C(entfeeorderstate.FieldPaymentState),
+		t.C(entfeeorderstate.FieldCancelState),
+	)
+	return nil
+}
+
+func (h *baseQueryHandler) queryJoinLedgerLock(s *sql.Selector) {
+	t := sql.Table(entorderlock.Table)
+	s.Join(t).
+		On(
+			s.C(entorderbase.FieldEntID),
+			t.C(entorderlock.FieldOrderID),
+		).
+		OnP(
+			sql.EQ(t.C(entorderlock.FieldLockType), types.OrderLockType_LockBalance.String()),
+		)
+	s.AppendSelect(
+		sql.As(t.C(entorderlock.FieldEntID), "ledger_lock_id"),
+	)
+}
+
+func (h *baseQueryHandler) queryJoinPaymentBase(s *sql.Selector) {
+	t := sql.Table(entpaymentbase.Table)
+	s.Join(t).
+		On(
+			s.C(entorderbase.FieldEntID),
+			t.C(entpaymentbase.FieldOrderID),
+		).
+		OnP(
+			sql.EQ(t.C(entpaymentbase.FieldObseleteState), types.PaymentObseleteState_PaymentObseleteNone.String()),
+		)
+	s.AppendSelect(
+		sql.As(t.C(entpaymentbase.FieldEntID), "payment_id"),
+	)
 }
 
 func (h *baseQueryHandler) queryJoin() {
 	h.stmSelect.Modify(func(s *sql.Selector) {
 		h.queryJoinMyself(s)
 		h.queryJoinFeeOrder(s)
+		h.queryJoinOrderStateBase(s)
+		h.queryJoinFeeOrderState(s)
+		h.queryJoinLedgerLock(s)
+		h.queryJoinPaymentBase(s)
 	})
 }
