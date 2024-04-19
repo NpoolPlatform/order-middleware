86e8496d2faff333d99facaf15fdf00d3aa450f4
Zhao KK <kikakkz@hotmail.com>
2024-04-19 09:55:09 +0000

    Add payment balance lock

---
+++ pkg/mw/payment/balance/lock/handler.go
@@ -0,0 +1,89 @@
+package paymentbalancelock
+
+import (
+	"context"
+	"fmt"
+
+	paymentbalancelockcrud "github.com/NpoolPlatform/order-middleware/pkg/crud/payment/balance/lock"
+
+	"github.com/google/uuid"
+)
+
+type Handler struct {
+	ID *uint32
+	paymentbalancelockcrud.Req
+}
+
+func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
+	handler := &Handler{}
+	for _, opt := range options {
+		if err := opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithID(u *uint32, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if u == nil {
+			if must {
+				return fmt.Errorf("invalid id")
+			}
+			return nil
+		}
+		h.ID = u
+		return nil
+	}
+}
+
+func WithEntID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid entid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.EntID = &_id
+		return nil
+	}
+}
+
+func WithPaymentID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid orderid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.PaymentID = &_id
+		return nil
+	}
+}
+
+func WithLedgerLockID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid couponid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.LedgerLockID = &_id
+		return nil
+	}
+}
