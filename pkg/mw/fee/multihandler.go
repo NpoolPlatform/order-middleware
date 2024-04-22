8f7102eed378c67a7a8e3c2340a185426245c3d6
Zhao KK <kikakkz@hotmail.com>
2024-04-22 08:39:03 +0000

    Add payment checker to fee order

--- pkg/mw/fee/multihandler.go
+++ pkg/mw/fee/multihandler.go
@@ -2,9 +2,13 @@ package feeorder
 
 import (
 	"context"
+	"fmt"
 
+	types "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
 	"github.com/NpoolPlatform/order-middleware/pkg/db"
 	"github.com/NpoolPlatform/order-middleware/pkg/db/ent"
+
+	"github.com/google/uuid"
 )
 
 type MultiHandler struct {
@@ -15,7 +19,76 @@ func (h *MultiHandler) AppendHandler(handler *Handler) {
 	h.Handlers = append(h.Handlers, handler)
 }
 
+func (h *MultiHandler) validatePaymentOrder() error {
+	paymentOrders := 0
+	for _, handler := range h.Handlers {
+		switch *handler.OrderStateBaseReq.PaymentType {
+		case types.PaymentType_PayWithBalanceOnly:
+			fallthrough //nolint
+		case types.PaymentType_PayWithTransferOnly:
+			fallthrough //nolint
+		case types.PaymentType_PayWithTransferAndBalance:
+			paymentOrders += 1
+		}
+	}
+	if paymentOrders != 1 {
+		return fmt.Errorf("invalid paymentorder")
+	}
+	return nil
+}
+
+func (h *MultiHandler) validatePayment() error {
+	paymentIDs := map[uuid.UUID]struct{}{}
+
+	for _, handler := range h.Handlers {
+		if len(paymentIDs) > 0 && handler.PaymentBaseReq.EntID == nil {
+			return fmt.Errorf("invalid paymentid")
+		}
+		if handler.PaymentBaseReq.EntID != nil {
+			paymentIDs[*handler.PaymentBaseReq.EntID] = struct{}{}
+		}
+		for _, balance := range handler.PaymentBalanceReqs {
+			if len(paymentIDs) > 0 && balance.PaymentID == nil {
+				return fmt.Errorf("invalid paymentid")
+			}
+			if balance.PaymentID != nil {
+				paymentIDs[*balance.PaymentID] = struct{}{}
+			}
+		}
+		for _, transfer := range handler.PaymentTransferReqs {
+			if len(paymentIDs) > 0 && transfer.PaymentID == nil {
+				return fmt.Errorf("invalid paymentid")
+			}
+			if transfer.PaymentID != nil {
+				paymentIDs[*transfer.PaymentID] = struct{}{}
+			}
+		}
+	}
+	if len(paymentIDs) > 1 {
+		return fmt.Errorf("invalid paymentid")
+	}
+	if len(paymentIDs) == 1 {
+		return nil
+	}
+
+	paymentID := uuid.New()
+	for _, handler := range h.Handlers {
+		handler.PaymentBaseReq.EntID = &paymentID
+		for _, balance := range handler.PaymentBalanceReqs {
+			balance.PaymentID = &paymentID
+		}
+		for _, transfer := range handler.PaymentTransferReqs {
+			transfer.PaymentID = &paymentID
+		}
+	}
+
+	return nil
+}
+
 func (h *MultiHandler) CreateFeeOrdersWithTx(ctx context.Context, tx *ent.Tx) error {
+	if err := h.validatePaymentOrder(); err != nil {
+		return err
+	}
 	for _, handler := range h.Handlers {
 		if err := handler.CreateFeeOrderWithTx(ctx, tx); err != nil {
 			return err
