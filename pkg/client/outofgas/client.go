d91ef666b0a4852d2d6ec5a7e08a1bd6fd5e8f93
Zhao KK <kikakkz@hotmail.com>
2024-04-22 11:00:41 +0000

    Add compensate and out of gas client api

--- pkg/client/outofgas/client.go
+++ pkg/client/outofgas/client.go
@@ -6,67 +6,25 @@ import (
 	"time"
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/order/mw/v1/outofgas"
-
 	servicename "github.com/NpoolPlatform/order-middleware/pkg/servicename"
+	"google.golang.org/grpc"
 )
 
-var timeout = 10 * time.Second
-
-type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
-
-func do(ctx context.Context, handler handler) (cruder.Any, error) {
-	_ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
-	if err != nil {
-		return nil, err
-	}
-
-	defer conn.Close()
-
-	cli := npool.NewMiddlewareClient(conn)
-
-	return handler(_ctx, cli)
-}
-
-func CreateOutOfGas(ctx context.Context, in *npool.OutOfGasReq) (*npool.OutOfGas, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.CreateOutOfGas(ctx, &npool.CreateOutOfGasRequest{
-			Info: in,
-		})
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.OutOfGas), nil
-}
-
-func UpdateOutOfGas(ctx context.Context, in *npool.OutOfGasReq) (*npool.OutOfGas, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.UpdateOutOfGas(ctx, &npool.UpdateOutOfGasRequest{
-			Info: in,
-		})
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.OutOfGas), nil
+func withClient(ctx context.Context, handler func(context.Context, npool.MiddlewareClient) (interface{}, error)) (interface{}, error) {
+	return grpc2.WithGRPCConn(
+		ctx,
+		servicename.ServiceDomain,
+		10*time.Second, //nolint
+		func(_ctx context.Context, conn *grpc.ClientConn) (interface{}, error) {
+			return handler(_ctx, npool.NewMiddlewareClient(conn))
+		},
+		grpc2.GRPCTAG,
+	)
 }
 
 func GetOutOfGas(ctx context.Context, id string) (*npool.OutOfGas, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	info, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetOutOfGas(ctx, &npool.GetOutOfGasRequest{
 			EntID: id,
 		})
@@ -81,10 +39,8 @@ func GetOutOfGas(ctx context.Context, id string) (*npool.OutOfGas, error) {
 	return info.(*npool.OutOfGas), nil
 }
 
-func GetOutOfGases(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.OutOfGas, uint32, error) {
-	total := uint32(0)
-
-	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+func GetOutOfGases(ctx context.Context, conds *npool.Conds, offset, limit int32) (infos []*npool.OutOfGas, total uint32, err error) {
+	_infos, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetOutOfGases(ctx, &npool.GetOutOfGasesRequest{
 			Conds:  conds,
 			Offset: offset,
@@ -93,33 +49,27 @@ func GetOutOfGases(ctx context.Context, conds *npool.Conds, offset, limit int32)
 		if err != nil {
 			return nil, err
 		}
-
 		total = resp.Total
-
 		return resp.Infos, nil
 	})
 	if err != nil {
 		return nil, 0, err
 	}
-	return infos.([]*npool.OutOfGas), total, nil
+	return _infos.([]*npool.OutOfGas), total, nil
 }
 
 func GetOutOfGasOnly(ctx context.Context, conds *npool.Conds) (*npool.OutOfGas, error) {
-	const limit = 2
-	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	infos, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetOutOfGases(ctx, &npool.GetOutOfGasesRequest{
 			Conds:  conds,
 			Offset: 0,
-			Limit:  limit,
+			Limit:  2,
 		})
 		if err != nil {
 			return nil, err
 		}
 		return resp.Infos, nil
 	})
-	if err != nil {
-		return nil, err
-	}
 	if err != nil {
 		return nil, err
 	}
@@ -132,26 +82,10 @@ func GetOutOfGasOnly(ctx context.Context, conds *npool.Conds) (*npool.OutOfGas, 
 	return infos.([]*npool.OutOfGas)[0], nil
 }
 
-func DeleteOutOfGas(ctx context.Context, in *npool.OutOfGasReq) (*npool.OutOfGas, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.DeleteOutOfGas(ctx, &npool.DeleteOutOfGasRequest{
-			Info: in,
-		})
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.OutOfGas), nil
-}
-
-func ExistOutOfGas(ctx context.Context, id string) (bool, error) {
-	exist, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+func ExistOutOfGas(ctx context.Context, entID string) (bool, error) {
+	exist, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.ExistOutOfGas(ctx, &npool.ExistOutOfGasRequest{
-			EntID: id,
+			EntID: entID,
 		})
 		if err != nil {
 			return false, err
@@ -166,7 +100,7 @@ func ExistOutOfGas(ctx context.Context, id string) (bool, error) {
 }
 
 func ExistOutOfGasConds(ctx context.Context, conds *npool.Conds) (bool, error) {
-	exist, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	exist, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.ExistOutOfGasConds(ctx, &npool.ExistOutOfGasCondsRequest{
 			Conds: conds,
 		})
--- pkg/client/powerrental/compensate/client.go
+++ pkg/client/powerrental/compensate/client.go
@@ -23,6 +23,24 @@ func withClient(ctx context.Context, handler func(context.Context, npool.Middlew
 	)
 }
 
+func CreateCompensate(ctx context.Context, in *npool.CompensateReq) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.CreateCompensate(ctx, &npool.CreateCompensateRequest{
+			Info: in,
+		})
+	})
+	return err
+}
+
+func UpdateCompensate(ctx context.Context, in *npool.CompensateReq) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.UpdateCompensate(ctx, &npool.UpdateCompensateRequest{
+			Info: in,
+		})
+	})
+	return err
+}
+
 func GetCompensate(ctx context.Context, id string) (*npool.Compensate, error) {
 	info, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetCompensate(ctx, &npool.GetCompensateRequest{
@@ -80,6 +98,25 @@ func GetCompensateOnly(ctx context.Context, conds *npool.Conds) (*npool.Compensa
 		return nil, fmt.Errorf("too many records")
 	}
 	return infos.([]*npool.Compensate)[0], nil
+}
+
+func DeleteCompensate(ctx context.Context, id *uint32, entID *string) error {
+	info, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		resp, err := cli.DeleteCompensate(ctx, &npool.DeleteCompensateRequest{
+			Info: &npool.CompensateReq{
+				ID:    id,
+				EntID: entID,
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Compensate), nil
 }
 
 func ExistCompensate(ctx context.Context, entID string) (bool, error) {
