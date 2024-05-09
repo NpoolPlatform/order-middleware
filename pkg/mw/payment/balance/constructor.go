57ac646037f6d2ef88e1a84fa032ba421485eff8
Zhao KK <kikakkz@hotmail.com>
2024-05-09 06:03:08 +0000

    If we don't have local currency, set it to 0

--- pkg/mw/payment/balance/constructor.go
+++ pkg/mw/payment/balance/constructor.go
@@ -29,9 +29,7 @@ func (h *Handler) ConstructCreateSQL() string {
 	_sql += comma + "coin_type_id"
 	_sql += comma + "amount"
 	_sql += comma + "coin_usd_currency"
-	if h.LocalCoinUSDCurrency != nil {
-		_sql += comma + "local_coin_usd_currency"
-	}
+	_sql += comma + "local_coin_usd_currency"
 	_sql += comma + "live_coin_usd_currency"
 	_sql += comma + "created_at"
 	_sql += comma + "updated_at"
@@ -50,6 +48,8 @@ func (h *Handler) ConstructCreateSQL() string {
 	_sql += fmt.Sprintf("%v'%v' as coin_usd_currency", comma, *h.CoinUSDCurrency)
 	if h.LocalCoinUSDCurrency != nil {
 		_sql += fmt.Sprintf("%v'%v' as local_coin_usd_currency", comma, *h.LocalCoinUSDCurrency)
+	} else {
+		_sql += fmt.Sprintf("%v'0' as local_coin_usd_currency", comma)
 	}
 	_sql += fmt.Sprintf("%v'%v' as live_coin_usd_currency", comma, *h.LiveCoinUSDCurrency)
 	_sql += fmt.Sprintf("%v%v as created_at", comma, now)
