9711d69253887b9afb0fd2a566455e06fa3a4564
Zhao KK <kikakkz@hotmail.com>
2024-05-17 08:26:00 +0000

    Fix: http://47.116.78.131:10083/issues/3352

--- api/fee/create.go
+++ api/fee/create.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	types "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
 	npool "github.com/NpoolPlatform/message/npool/order/mw/v1/fee"
 
 	feeorder1 "github.com/NpoolPlatform/order-middleware/pkg/mw/fee"
@@ -21,6 +22,18 @@ func (s *Server) CreateFeeOrder(ctx context.Context, in *npool.CreateFeeOrderReq
 			"In", in,
 		)
 		return &npool.CreateFeeOrderResponse{}, status.Error(codes.Aborted, "invalid argument")
+	}
+	switch req.GetPaymentType() {
+	case types.PaymentType_DefaultPaymentType:
+		fallthrough //nolint
+	case types.PaymentType_PayWithOtherOrder:
+		fallthrough //nolint
+	case types.PaymentType_PayWithParentOrder:
+		logger.Sugar().Errorw(
+			"CreateFeeOrder",
+			"In", in,
+		)
+		return &npool.CreateFeeOrderResponse{}, status.Error(codes.Aborted, "invalid paymenttype")
 	}
 	handler, err := feeorder1.NewHandler(
 		ctx,
