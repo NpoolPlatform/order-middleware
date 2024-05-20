144cdacf9c16c80631bd0c15766417638b864537
Zhao KK <kikakkz@hotmail.com>
2024-05-20 06:15:21 +0000

    Formalize payment obselete state

--- pkg/mw/payment/query.go
+++ pkg/mw/payment/query.go
@@ -4,16 +4,94 @@ import (
 	"context"
 
 	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	types "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
 	npool "github.com/NpoolPlatform/message/npool/order/mw/v1/payment"
+	paymentbalancecrud "github.com/NpoolPlatform/order-middleware/pkg/crud/payment/balance"
+	paymenttransfercrud "github.com/NpoolPlatform/order-middleware/pkg/crud/payment/transfer"
 	"github.com/NpoolPlatform/order-middleware/pkg/db"
 	"github.com/NpoolPlatform/order-middleware/pkg/db/ent"
+	entpaymentbalance "github.com/NpoolPlatform/order-middleware/pkg/db/ent/paymentbalance"
+	entpaymenttransfer "github.com/NpoolPlatform/order-middleware/pkg/db/ent/paymenttransfer"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 )
 
 type queryHandler struct {
 	*baseQueryHandler
-	stmCount *ent.PaymentBaseSelect
-	infos    []*npool.Payment
-	total    uint32
+	stmCount         *ent.PaymentBaseSelect
+	infos            []*npool.Payment
+	paymentBalances  []*npool.PaymentBalanceInfo
+	paymentTransfers []*npool.PaymentTransferInfo
+	total            uint32
+}
+
+func (h *queryHandler) queryPaymentBalances(ctx context.Context, cli *ent.Client) error {
+	paymentIDs := func() (uids []uuid.UUID) {
+		for _, info := range h.infos {
+			if _, err := uuid.Parse(info.EntID); err != nil {
+				continue
+			}
+			uids = append(uids, uuid.MustParse(info.EntID))
+		}
+		return
+	}()
+
+	stm, err := paymentbalancecrud.SetQueryConds(
+		cli.PaymentBalance.Query(),
+		&paymentbalancecrud.Conds{
+			PaymentIDs: &cruder.Cond{Op: cruder.IN, Val: paymentIDs},
+		},
+	)
+	if err != nil {
+		return wlog.WrapError(err)
+	}
+
+	return stm.Select(
+		entpaymentbalance.FieldPaymentID,
+		entpaymentbalance.FieldCoinTypeID,
+		entpaymentbalance.FieldAmount,
+		entpaymentbalance.FieldCoinUsdCurrency,
+		entpaymentbalance.FieldLocalCoinUsdCurrency,
+		entpaymentbalance.FieldLiveCoinUsdCurrency,
+		entpaymentbalance.FieldCreatedAt,
+	).Scan(ctx, &h.paymentBalances)
+}
+
+func (h *queryHandler) queryPaymentTransfers(ctx context.Context, cli *ent.Client) error {
+	paymentIDs := func() (uids []uuid.UUID) {
+		for _, info := range h.infos {
+			if _, err := uuid.Parse(info.EntID); err != nil {
+				continue
+			}
+			uids = append(uids, uuid.MustParse(info.EntID))
+		}
+		return
+	}()
+
+	stm, err := paymenttransfercrud.SetQueryConds(
+		cli.PaymentTransfer.Query(),
+		&paymenttransfercrud.Conds{
+			PaymentIDs: &cruder.Cond{Op: cruder.IN, Val: paymentIDs},
+		},
+	)
+	if err != nil {
+		return wlog.WrapError(err)
+	}
+
+	return stm.Select(
+		entpaymenttransfer.FieldPaymentID,
+		entpaymenttransfer.FieldCoinTypeID,
+		entpaymenttransfer.FieldAmount,
+		entpaymenttransfer.FieldAccountID,
+		entpaymenttransfer.FieldStartAmount,
+		entpaymenttransfer.FieldCoinUsdCurrency,
+		entpaymenttransfer.FieldLocalCoinUsdCurrency,
+		entpaymenttransfer.FieldLiveCoinUsdCurrency,
+		entpaymenttransfer.FieldFinishAmount,
+		entpaymenttransfer.FieldCreatedAt,
+	).Scan(ctx, &h.paymentTransfers)
 }
 
 func (h *queryHandler) scan(ctx context.Context) error {
@@ -21,7 +99,82 @@ func (h *queryHandler) scan(ctx context.Context) error {
 }
 
 func (h *queryHandler) formalize() {
+	paymentBalances := map[string][]*npool.PaymentBalanceInfo{}
+	paymentTransfers := map[string][]*npool.PaymentTransferInfo{}
+	for _, paymentBalance := range h.paymentBalances {
+		paymentBalances[paymentBalance.PaymentID] = append(paymentBalances[paymentBalance.PaymentID], paymentBalance)
+		paymentBalance.Amount = func() string { amount, _ := decimal.NewFromString(paymentBalance.Amount); return amount.String() }()
+		paymentBalance.CoinUSDCurrency = func() string {
+			amount, _ := decimal.NewFromString(paymentBalance.CoinUSDCurrency)
+			return amount.String()
+		}()
+		paymentBalance.LocalCoinUSDCurrency = func() string {
+			amount, _ := decimal.NewFromString(paymentBalance.LocalCoinUSDCurrency)
+			return amount.String()
+		}()
+		paymentBalance.LiveCoinUSDCurrency = func() string {
+			amount, _ := decimal.NewFromString(paymentBalance.LiveCoinUSDCurrency)
+			return amount.String()
+		}()
+	}
+	for _, paymentTransfer := range h.paymentTransfers {
+		paymentTransfers[paymentTransfer.PaymentID] = append(paymentTransfers[paymentTransfer.PaymentID], paymentTransfer)
+		paymentTransfer.Amount = func() string { amount, _ := decimal.NewFromString(paymentTransfer.Amount); return amount.String() }()
+		paymentTransfer.StartAmount = func() string { amount, _ := decimal.NewFromString(paymentTransfer.StartAmount); return amount.String() }()
+		paymentTransfer.FinishAmount = func() string {
+			amount, _ := decimal.NewFromString(paymentTransfer.FinishAmount)
+			return amount.String()
+		}()
+		paymentTransfer.CoinUSDCurrency = func() string {
+			amount, _ := decimal.NewFromString(paymentTransfer.CoinUSDCurrency)
+			return amount.String()
+		}()
+		paymentTransfer.LocalCoinUSDCurrency = func() string {
+			amount, _ := decimal.NewFromString(paymentTransfer.LocalCoinUSDCurrency)
+			return amount.String()
+		}()
+		paymentTransfer.LiveCoinUSDCurrency = func() string {
+			amount, _ := decimal.NewFromString(paymentTransfer.LiveCoinUSDCurrency)
+			return amount.String()
+		}()
+	}
+	for _, info := range h.infos {
+		info.ObseleteState = types.PaymentObseleteState(types.PaymentObseleteState_value[info.ObseleteStateStr])
+		info.PaymentBalances = paymentBalances[info.EntID]
+		info.PaymentTransfers = paymentTransfers[info.EntID]
+	}
+}
 
+func (h *Handler) GetPayment(ctx context.Context) (*npool.Payment, error) {
+	handler := &queryHandler{
+		baseQueryHandler: &baseQueryHandler{
+			Handler: h,
+		},
+	}
+	if err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryPaymentBase(cli); err != nil {
+			return wlog.WrapError(err)
+		}
+		handler.stmSelect.Offset(0).Limit(2)
+		if err := handler.scan(_ctx); err != nil {
+			return wlog.WrapError(err)
+		}
+		if err := handler.queryPaymentBalances(_ctx, cli); err != nil {
+			return wlog.WrapError(err)
+		}
+		return handler.queryPaymentTransfers(_ctx, cli)
+	}); err != nil {
+		return nil, wlog.WrapError(err)
+	}
+	if len(handler.infos) == 0 {
+		return nil, nil
+	}
+	if len(handler.infos) > 1 {
+		return nil, wlog.Errorf("too many records")
+	}
+
+	handler.formalize()
+	return handler.infos[0], nil
 }
 
 func (h *Handler) GetPayments(ctx context.Context) (infos []*npool.Payment, total uint32, err error) {
@@ -50,9 +203,10 @@ func (h *Handler) GetPayments(ctx context.Context) (infos []*npool.Payment, tota
 		if err := handler.scan(_ctx); err != nil {
 			return wlog.WrapError(err)
 		}
-		// TODO: get payment transfers
-		// TODO: get payment balances
-		return nil
+		if err := handler.queryPaymentBalances(_ctx, cli); err != nil {
+			return wlog.WrapError(err)
+		}
+		return handler.queryPaymentTransfers(_ctx, cli)
 	}); err != nil {
 		return nil, 0, wlog.WrapError(err)
 	}
